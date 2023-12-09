ddf2ca01c46b631ebe123ee77bb6ff01b02d8381
Tim van Meijel <t.m.m.v.meijel@student.tue.nl>
2023-12-08 19:15:37 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+from setuptools import find_packages, setup
+
+VERSION = '1.0.1'
+
+INSTALL_REQUIRES = (
+    [
+        'mujoco >= 2.2.0',
+        'glfw >= 2.5.0',
+        'imageio'
+    ]
+)
+
+setup(
+    name='mujoco-python-engine',
+    version=VERSION,
+    author='Jack Xu',
+    author_email='projectbyjx@gmail.com',
+    url='https://github.com/UW-Advanced-Robotics-Lab/uwarl-mujoco-python-engine',
+    description='Interactive engine for MuJoCo Python',
+    long_description='Interactive engine for MuJoCo Python, inspired by interactive mujoco-python-engine and mujoco-py',
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    zip_safe=False,
+)
