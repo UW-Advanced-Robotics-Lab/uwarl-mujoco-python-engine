d3eb72351158f02d11749546513ae5b8fe3c1881
arnab <ajoardar@uwaterloo.ca>
2024-03-25 14:37:33 -0400

    - Modified the engine-file to ignore commands about sending control-commands to the mobile-abse of the Summit. New engine file for peg-in-hole demo is  `engine_peg_in_hole.py`, - Modified the state-publisher-file to sue the new link and joint-names for the peg-in-hole demo, over the links and joints of the Summit-WAM-Cart demo. New state-publisher file for the peg-in-hole demo is `state_pub_mujoco_peg_in_hole.py`, - Modified the `mujoco_engine/core_engine/__init__.py` to initialize the engine and state-publisher files corresponding to either the `peg-in-hole demo` or the `Summit-WAM-Cart demo`.

--- mujoco_engine/__init__.py
+++ mujoco_engine/__init__.py
@@ -1,1 +1,2 @@
+# from mujoco_engine import engine_peg_in_hole
 # from mujoco_engine import engine
--- mujoco_engine/core_engine/__init__.py
+++ mujoco_engine/core_engine/__init__.py
@@ -1,4 +1,6 @@
-from mujoco_engine.core_engine.engine import Mujoco_Engine
-from mujoco_engine.core_engine.engine import MuJoCo_Engine_InterruptException
+from mujoco_engine.core_engine.engine_peg_in_hole import Mujoco_Engine
+from mujoco_engine.core_engine.engine_peg_in_hole import MuJoCo_Engine_InterruptException
+# from mujoco_engine.core_engine.engine import Mujoco_Engine
+# from mujoco_engine.core_engine.engine import MuJoCo_Engine_InterruptException
 
 from mujoco_engine.core_engine import *
