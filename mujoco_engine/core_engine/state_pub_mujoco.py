c74a1d5aa9605fc3150c1d01c8cabc75e7c25e75
arnab <ajoardar@uwaterloo.ca>
2024-05-15 10:52:24 -0400

    - Updated the Cart DOF to allow it to be lifted up and rotated freely

--- mujoco_engine/core_engine/state_pub_mujoco.py
+++ mujoco_engine/core_engine/state_pub_mujoco.py
@@ -27,7 +27,7 @@ class StatePublisherMujoco(object):
                             'bhand/f2/dist', 'bhand/f3/med', 'bhand/f3/dist', 'smt/orie/z', 'smt/pose/x', 'smt/pose/y', 
                             'smt/whl_LF', 'smt/whl_LR', 'smt/whl_RF', 'smt/whl_RR', 'smt/world_x', 'smt/world_y', 'smt/world_z', 
                             'wagon/LF', 'wagon/LF/whl', 'wagon/LR/whl', 'wagon/RF', 'wagon/RF/whl', 'wagon/RR/whl', 'wagon/handle','wagon/handle2',
-                            'wagon/world_x', 'wagon/world_y', 'wagon/world_z', 
+                            'wagon/slide/world_x', 'wagon/slide/world_y', 'wagon/slide/world_z', 'wagon/hinge/world_y', 'wagon/hinge/world_z',
                             'wam/J1','wam/J2', 'wam/J3', 'wam/J4', 'wam/J5', 'wam/J6', 'wam/J7']
         
         self.linklist =  ['smt/base_link', 'smt/whl/LF_link', 'smt/whl/LR_link', 'smt/whl/RF_link', 'smt/whl/RR_link', 
