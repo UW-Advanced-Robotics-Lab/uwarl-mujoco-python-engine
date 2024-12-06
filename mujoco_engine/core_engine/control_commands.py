0ab053ac4c0b13d33d3319610cf092ba5cf591fe
arnab <ajoardar@uwaterloo.ca>
2024-12-06 12:22:59 -0500

    - Instead of reading the MB yaw-joint, which will always be 0, regardless of what orientation it is imported in, we replace it with the yaw-angle obtained from the pose-matrix of the body.     - This only works if we intend to keep a fixed home-position in this (sim-)world.

--- mujoco_engine/core_engine/control_commands.py
+++ mujoco_engine/core_engine/control_commands.py
@@ -41,10 +41,10 @@ class ControlCommand(object):
         self.vel_base = [msg.linear.x, msg.linear.y, msg.angular.z]
 
     # PID loop to control x velocity of non-holonomic-robot in map frame
-    def velx_PID(self, Kp, Ki, Kd, CP, base_name):
+    def velx_PID(self, Kp, Ki, Kd, CP, base_name, yaw_theta):
 
         # Convert reference velocities into mapframe
-        SP = self.vel_base[0]*cos(self.mj_data_control.joint(base_name+'/orie/z').qpos[0])+self.vel_base[1]*-sin(self.mj_data_control.joint(base_name+'/orie/z').qpos[0])
+        SP = self.vel_base[0]*cos(yaw_theta)+self.vel_base[1]*-sin(yaw_theta)
 
         t = rospy.Time().now().to_time()
         ex = float(SP - CP)
@@ -59,16 +59,18 @@ class ControlCommand(object):
 
         control = P+self.Ix+D
 
+        # print("Set-point - Control-point X-axis: "+str(ex))
+        # print("Control-command X-axis: "+str(control))
         # Set control commands in mj_data
         self.mj_data_control.actuator(base_name+'/pose/x').ctrl = control
 
 
     # PID loop to control y velocity of holonomic-robot in map frame
-    def vely_PID(self, Kp, Ki, Kd, CP, base_name):
+    def vely_PID(self, Kp, Ki, Kd, CP, base_name, yaw_theta):
 
         # Convert reference velocities into mapframe
-        SP = self.vel_base[0]*sin(self.mj_data_control.joint(base_name+'/orie/z').qpos[0])+self.vel_base[1]*cos(self.mj_data_control.joint(base_name+'/orie/z').qpos[0])
-
+        SP = self.vel_base[0]*sin(yaw_theta)+self.vel_base[1]*cos(yaw_theta)
+        # print("Yaw-orientation: "+str(yaw_theta))
         t = rospy.Time().now().to_time()
         ey = float(SP - CP)
 
@@ -82,6 +84,10 @@ class ControlCommand(object):
 
         control = P+self.Iy+D
 
+        # print("Set-point Y-axis: "+str(SP))
+        # print("Control-point Y-axis: "+str(CP))
+        # print("Set-point - Control-point Y-axis: "+str(ey))
+        # print("Control-command Y-axis: "+str(control))
         # Set control commands in mj_data
         self.mj_data_control.actuator(base_name+'/pose/y').ctrl = control
 
