ee2693f4913bc6b4910fb484e56c67de5545d2aa
arnab <ajoardar@uwaterloo.ca>
2025-05-21 09:55:20 -0400

    - Added a function to perform the velocity following for all twist components and record the computation on a topic.

--- mujoco_engine/core_engine/control_commands.py
+++ mujoco_engine/core_engine/control_commands.py
@@ -7,6 +7,7 @@ import numpy as np
 import rospy
 from math import pi, sin, cos
 from geometry_msgs.msg import Twist
+from uwarl_mujoco_ros_msgs.msg import MB_pid_effort
 from sensor_msgs.msg import JointState
 
 
@@ -21,16 +22,16 @@ class ControlCommand(object):
         self.mj_data_control = mj_data
 
         # Initialize PID variables for base velocity effort control
-        self.Ix = 0.0
-        self.ex_last = 0.0
+        self.I_x = 0.0
+        self.e_x_last = 0.0
         self.last_time_x = rospy.Time().now().to_time()
 
-        self.Iy = 0.0
-        self.ey_last = 0.0
+        self.I_y = 0.0
+        self.e_y_last = 0.0
         self.last_time_y = rospy.Time().now().to_time()
 
-        self.Itheta = 0.0
-        self.etheta_last = 0.0
+        self.I_theta = 0.0
+        self.e_theta_last = 0.0
         self.last_time_theta = rospy.Time().now().to_time()
 
         self.vel_base = [0.0, 0.0, 0.0]
@@ -51,6 +52,15 @@ class ControlCommand(object):
         self.last_time_whl_smt = rospy.Time().now().to_time()
         self.I_whl_smt = np.zeros((4,1))
 
+        # Record MB PID contribution
+        self.pub_base_velocity = rospy.Publisher(
+            '/mb_pid_contri',
+            MB_pid_effort,
+            queue_size=1
+        )
+        # Keep track of the sequence
+        self.mb_pid_seq_ind = 0
+
 
     def vel_base_callback(self, msg):
         # Store data in array to be used by PID control
@@ -64,17 +74,17 @@ class ControlCommand(object):
         SP = self.vel_base[0]
 
         t = rospy.Time().now().to_time()
-        ex = float(SP - CP)
+        e_x = float(SP - CP)
 
         # Compute PID variables
-        P = ex*Kp
-        self.Ix += Ki*ex*(t-self.last_time_x+0.0001) # Make sure it is never 0.0
-        D = Kd*(ex-self.ex_last)/(t-self.last_time_x+0.0001) # Make sure it is never 0.0
+        P = e_x*Kp
+        self.I_x += Ki*e_x*(t-self.last_time_x+0.0001) # Make sure it is never 0.0
+        D = Kd*(e_x-self.e_x_last)/(t-self.last_time_x+0.0001) # Make sure it is never 0.0
 
-        self.ex_last = ex
+        self.e_x_last = e_x
         self.last_time_x = t
 
-        control = P+self.Ix+D
+        control = P+self.I_x+D
 
         # Set control commands in mj_data
         self.mj_data_control.actuator(base_name+'/pose/x').ctrl = control
@@ -86,17 +96,17 @@ class ControlCommand(object):
         # Convert reference velocities into mapframe
         SP = self.vel_base[1]
         t = rospy.Time().now().to_time()
-        ey = float(SP - CP)
+        e_y = float(SP - CP)
 
         # Compute PID variables
-        P = ey*Kp
-        self.Iy += Ki*ey*(t-self.last_time_y+0.0001)
-        D = Kd*(ey-self.ey_last)/(t-self.last_time_y+0.0001)
+        P = e_y*Kp
+        self.I_y += Ki*e_y*(t-self.last_time_y+0.0001)
+        D = Kd*(e_y-self.e_y_last)/(t-self.last_time_y+0.0001)
 
-        self.ey_last = ey
+        self.e_y_last = e_y
         self.last_time_y = t
 
-        control = P+self.Iy+D
+        control = P+self.I_y+D
 
         # Set control commands in mj_data
         self.mj_data_control.actuator(base_name+'/pose/y').ctrl = control
@@ -108,20 +118,93 @@ class ControlCommand(object):
         SP = self.vel_base[2]
 
         t = rospy.Time().now().to_time()
-        etheta = float(SP - CP)
+        e_theta = float(SP - CP)
 
         # Compute PID variables
-        P = etheta*Kp
-        self.Itheta += Ki*etheta*(t-self.last_time_theta+0.0001)
-        D = Kd*(etheta-self.etheta_last)/(t-self.last_time_theta+0.0001)
+        P = e_theta*Kp
+        self.I_theta += Ki*e_theta*(t-self.last_time_theta+0.0001)
+        D = Kd*(e_theta-self.e_theta_last)/(t-self.last_time_theta+0.0001)
 
-        self.etheta_last = etheta
+        self.e_theta_last = e_theta
         self.last_time_theta = t
 
-        control = P+self.Itheta+D
+        control = P+self.I_theta+D
 
         # Set control commands in mj_data
         self.mj_data_control.actuator(base_name+'/orie/z').ctrl = control
+    
+    def vel_PID(self, Kp, Ki, Kd, CP, base_name):
+
+        SP_x = self.vel_base[0]
+        SP_y = self.vel_base[1]
+        SP_theta = self.vel_base[2]
+
+        CP_x = CP[0]
+        CP_y = CP[1]
+        CP_theta = CP[2]
+
+        t = rospy.Time().now().to_time()
+        e_x = float(SP_x - CP_x)
+        e_y = float(SP_y - CP_y)
+        e_theta = float(SP_theta - CP_theta)
+
+        # Compute PID variables
+        P_x = e_x*Kp[0]
+        P_y = e_y*Kp[1]
+        P_theta = e_theta*Kp[2]
+
+        self.I_x += Ki[0]*e_x*(t-self.last_time_theta+0.0001)
+        self.I_y += Ki[1]*e_y*(t-self.last_time_theta+0.0001)
+        self.I_theta += Ki[2]*e_theta*(t-self.last_time_theta+0.0001)
+
+        D_x = Kd[0]*(e_x-self.e_x_last)/(t-self.last_time_theta+0.0001)
+        D_y = Kd[1]*(e_y-self.e_y_last)/(t-self.last_time_theta+0.0001)
+        D_theta = Kd[2]*(e_theta-self.e_theta_last)/(t-self.last_time_theta+0.0001)
+
+        self.e_x_last = e_x
+        self.e_y_last = e_y
+        self.e_theta_last = e_theta
+
+        self.last_time_theta = t
+
+        control_x = P_x+self.I_x+D_x
+        control_y = P_y+self.I_y+D_y
+        control_theta = P_theta+self.I_theta+D_theta
+
+        # Set control commands in mj_data
+        self.mj_data_control.actuator(base_name+'/pose/x').ctrl = control_x
+        self.mj_data_control.actuator(base_name+'/pose/y').ctrl = control_y
+        self.mj_data_control.actuator(base_name+'/orie/z').ctrl = control_theta
+
+        # Record the PID contributions
+        mb_pid_effort_obj = MB_pid_effort()
+        mb_pid_effort_obj.header.seq = self.mb_pid_seq_ind
+        mb_pid_effort_obj.header.stamp = rospy.Time.now()
+        mb_pid_effort_obj.header.frame_id = "global-frame"
+        # Link name
+        mb_pid_effort_obj.link_name = base_name
+        # Current MB vel
+        mb_pid_effort_obj.curr_mb_vel.append(CP_x)
+        mb_pid_effort_obj.curr_mb_vel.append(CP_y)
+        mb_pid_effort_obj.curr_mb_vel.append(CP_theta)
+        # Set-point MB vel
+        mb_pid_effort_obj.dem_mb_vel.append(SP_x)
+        mb_pid_effort_obj.dem_mb_vel.append(SP_y)
+        mb_pid_effort_obj.dem_mb_vel.append(SP_theta)
+        # P-contribution
+        mb_pid_effort_obj.p_contribution.append(P_x)
+        mb_pid_effort_obj.p_contribution.append(P_y)
+        mb_pid_effort_obj.p_contribution.append(P_theta)
+        # I-contribution
+        mb_pid_effort_obj.i_contribution.append(self.I_x)
+        mb_pid_effort_obj.i_contribution.append(self.I_y)
+        mb_pid_effort_obj.i_contribution.append(self.I_theta)
+        # D-contribution
+        mb_pid_effort_obj.d_contribution.append(D_x)
+        mb_pid_effort_obj.d_contribution.append(D_y)
+        mb_pid_effort_obj.d_contribution.append(D_theta)
+
+        self.mb_pid_seq_ind +=1
     
     # PID loop to control wheel velocities
     def wheel_PID(self, Kp, Ki, Kd, CP, base_name):
@@ -132,7 +215,7 @@ class ControlCommand(object):
 
         # Wheel velocity: Set point
         whl_vel_SP = np.zeros((4,1))
-        if(base_name is "smt"):
+        if(base_name == "smt"):
             whl_vel_SP = np.matmul(self.h_mat_smt,mb_twist)
 
         t = rospy.Time().now().to_time()
